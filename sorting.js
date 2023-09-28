531d4e267245bf1b73c052c8631a76df859889f9
Prashanth <92428506+prassu10@users.noreply.github.com>
2023-09-28 12:46:30 +0530

    Update sorting.js

--- sorting.js
+++ sorting.js
@@ -11,14 +11,18 @@ function disableSortingBtn(){
     document.querySelector(".bubbleSort").disabled = true;
     document.querySelector(".selectionSort").disabled = true;
     document.querySelector(".insertionSort").disabled = true;
+    document.querySelector(".mergeSort").disabled = true;
     document.querySelector(".quickSort").disabled = true;
+    document.querySelector(".heapSort").disabled = true;
 }
 
 function enableSortingBtn(){
     document.querySelector(".bubbleSort").disabled = false;
     document.querySelector(".selectionSort").disabled = false;
     document.querySelector(".insertionSort").disabled = false;
+    document.querySelector(".mergeSort").disabled = true;
     document.querySelector(".quickSort").disabled = false;
+    document.querySelector(".heapSort").disabled = true;
 }
 
 
